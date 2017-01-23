29ec4887ef9549acdcfabc86a805dc07615e84d4
khaledIgou <bk_igoulalene@esi.dz>
2017-01-24 00:51:05 +0100

    Add xmcda objects

--- src/main/java/io/github/oliviercailloux/y2016/xmcda/servlets/ServletDeTest.java
+++ src/main/java/io/github/oliviercailloux/y2016/xmcda/servlets/ServletDeTest.java
@@ -2,13 +2,22 @@ package io.github.oliviercailloux.y2016.xmcda.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Date;
+import io.github.oliviercailloux.y2016.xmcda.objects.Alternative;
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 /**
  * Servlet implementation class ServletDeTest
  */
+@WebServlet("/ServletDeTest")
 public class ServletDeTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -23,8 +32,37 @@ public class ServletDeTest extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @SuppressWarnings("deprecation")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter printWriter= response.getWriter();
+		printWriter.println("<h2>");
+		printWriter.println("Entrée dans la servlet à "
+		+ new Date().toLocaleString());
+		printWriter.println("</h2>");
+		
+		Alternative alternative = new Alternative();
+		  alternative.setId("a01");
+		  alternative.setDate("12/02/2017");
+
+		  try {
+
+			File file = new File("C:\\file.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Alternative.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+			// output pretty printed
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(alternative, file);
+			jaxbMarshaller.marshal(alternative, System.out);
+
+		      } catch (JAXBException e) {
+			e.printStackTrace();
+		      }
+
+
+		
 	}
 
 	/**
