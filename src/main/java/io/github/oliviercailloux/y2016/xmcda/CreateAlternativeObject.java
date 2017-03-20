4596df9633f33452edc1b9127ffbcb5ed06ac908
Mansour NIANG <smansour.niang@gmail.com>
2017-03-20 23:01:08 +0100

    Create objects with POST and GET method

--- src/main/java/io/github/oliviercailloux/y2016/xmcda/CreateAlternativeObject.java
+++ src/main/java/io/github/oliviercailloux/y2016/xmcda/CreateAlternativeObject.java
@@ -52,7 +52,8 @@ public class CreateAlternativeObject extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String alternative = request.getParameter("alternative");		
+		String alternative = request.getParameter("alternative");	
+		
 		JAXBContext jc = null;
 		try {
 			jc = JAXBContext.newInstance(Alternative.class);
@@ -127,7 +128,75 @@ public class CreateAlternativeObject extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-					
+		String alternative = request.getParameter("alternative");	
+		
+		JAXBContext jc = null;
+		try {
+			jc = JAXBContext.newInstance(Alternative.class);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		Marshaller marshaller = null;
+		try {
+			marshaller = jc.createMarshaller();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		final ObjectFactory f = new ObjectFactory();
+
+		final XmlSchema annotation = ObjectFactory.class.getPackage().getAnnotation(XmlSchema.class);
+		final String namespace = annotation.namespace();
+
+		final Alternative alt = f.createAlternative();
+		alt.setId(alternative);
+
+		final QName altQName = new QName(namespace, "alternative", "xs");
+		final JAXBElement<Alternative> altEl = new JAXBElement<>(altQName, Alternative.class, alt);
+
+		final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+		final Document doc = docBuilder.newDocument();
+		docFactory.setNamespaceAware(true);
+		final Element rootElement = doc.createElementNS("namespace", "m:Alternative");
+		rootElement.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, "xmlns:xs", namespace);
+		doc.appendChild(rootElement);
+
+		try {
+			marshaller.marshal(altEl, rootElement);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = null;
+		try {
+			transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		}
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		/** Inelegant. (Impl. dependent.) */
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+		final DOMSource source = new DOMSource(doc);
+		// final StreamResult result = new StreamResult(new File("file.xml"));
+		StringWriter writer = new StringWriter();
+		final StreamResult resultStream = new StreamResult(writer);
+		try {
+			transformer.transform(source, resultStream);
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+		String result = writer.toString(); 
+		
+		request.setAttribute("result", result.replace("\"","&quot;").replace("<","&lt;").replace(">","&gt;"));
+		//request.setAttribute("result", result);
+		request.getServletContext().getRequestDispatcher("/alternativeCreated.jsp").forward(request, response);	
 		
 	}
 
